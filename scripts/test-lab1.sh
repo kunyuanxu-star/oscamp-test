8accd797694181aba66fe632c6180e903adc6c35
yjymosheng <yjymosheng@gmail.com>
2025-04-12 17:48:31 +0800

    add

--- scripts/test-lab1.sh
+++ scripts/test-lab1.sh
@@ -3,14 +3,15 @@
 TMP_FILE=g.txt
 TIMEOUT_DURATION=20
 
-echo "=== 开始测试 lab1 ==="
-工作目录的原因,这个得在ci设置为arceos目录下运行
-pwd
+# echo "=== 开始测试 lab1 ==="
+# 工作目录的原因,这个得在ci设置为arceos目录下运行
+# pwd
 
-which cargo
-cargo --version
+# which cargo
+# cargo --version
 
-timeout --foreground ${TIMEOUT_DURATION}s ./verify_lab1.sh >"$TMP_FILE" 
+timeout --foreground ${TIMEOUT_DURATION}s ./verify_lab1.sh >"$TMP_FILE" 2>/dev/null
+# exit_code=$?
 
 # echo "timeout exit_code = $exit_code"
 
@@ -23,7 +24,7 @@ score=$(grep "Indicator:" $TMP_FILE | tail -n1 | sed -E 's/.*Indicator: ([0-9]+)
 score=${score:-0}
 # fi
 
-cat $TMP_FILE
+# cat $TMP_FILE
 
 rm $TMP_FILE
 
