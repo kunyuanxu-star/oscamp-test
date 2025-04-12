22018ac060bbc651e107e7b8e5c419c2251ed606
yjymosheng <yjymosheng@gmail.com>
2025-04-12 16:07:33 +0800

    add

--- scripts/test-lab1.sh
+++ scripts/test-lab1.sh
@@ -7,20 +7,20 @@ TIMEOUT_DURATION=20
 # 工作目录的原因,这个得在ci设置为arceos目录下运行
 
 timeout --foreground ${TIMEOUT_DURATION}s ./verify_lab1.sh >"$TMP_FILE" 2>/dev/null
-exit_code=$?
+# exit_code=$?
 
 # echo "timeout exit_code = $exit_code"
 
-if [ "$exit_code" -eq 124 ]; then
-    score=99999
-else
+# if [ "$exit_code" -eq 124 ]; then
+#     score=99999
+# else
     score=$(grep "Indicator:" $TMP_FILE | tail -n1 | sed -E 's/.*Indicator: ([0-9]+).*/\1/')
     # echo "得分为: $score"
 
     score=${score:-0}
-fi
+# fi
 
-rm $TMP_FILE
+# rm $TMP_FILE
 
 echo "$score"
 
